df6d76c6fa587be78cd68be7f216697acece660c
gitriond2 <pinki.por.100@gmail.com>
2025-01-07 17:41:09 -0300

    Model1.2.1

--- HealthClaimsVerification/obj/Debug/net7.0/HealthClaimsVerification.AssemblyInfo.cs
+++ HealthClaimsVerification/obj/Debug/net7.0/HealthClaimsVerification.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HealthClaimsVerification")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f38080b3e3101b305f3655114c57ad120b696c59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce9d9ced4421661a9cdc0ee97b1144b56b779c7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("HealthClaimsVerification")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HealthClaimsVerification")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
